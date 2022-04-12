4a6b7fc4c51b0d9af3ac78983f2b55a60f28bc87
Volkov Alexander <volkov.525@gmail.com>
2022-04-12 16:40:57 +0300

    setup with bash script

---
+++ setup-ts.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+npm i -D eslint eslint-plugin-vue @vue/eslint-config-typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin prettier
+
+cat > .eslintrc.js <<EOF
+module.exports = {
+  root: true,
+  env: {
+      browser: true,
+      es2021: true,
+      node: true
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+		"@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
+  ],
+  parserOptions: {
+     ecmaVersion: 2021
+  },
+  plugins: [
+  ],
+  rules: {
+  }
+}
+EOF
+
+cat > .prettierrc.js <<EOF
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  tabWidth: 2,
+  useTabs: true,
+  trailingComma: "es5",
+  singleQuote: true
+}
+EOF
+
+cat > vite.config.ts <<EOF
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, '/src')
+		}
+	},
+})
+EOF
+
+cat > tsconfig.json <<EOF
+{
+  "compilerOptions": {
+    "target": "es5",
+    "module": "esnext",
+    "strict": true,
+    "jsx": "preserve",
+    "importHelpers": true,
+    "moduleResolution": "node",
+    "skipLibCheck": true,
+    "esModuleInterop": true,
+    "allowSyntheticDefaultImports": true,
+    "sourceMap": true,
+    "baseUrl": ".",
+    "paths": {
+      "/@/*": [ // / to begin with.
+        "src/*"
+      ]
+    },
+    "lib": [
+      "esnext",
+      "dom",
+      "dom.iterable",
+      "scripthost"
+    ]
+  },
+  "include": [
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "src/**/*.vue",
+  ],
+  "exclude": [
+    "node_modules"
+  ]
+}
+EOF
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template lang="pug">
+h1 {{ msg }}
+
+<button type="button" @click="count++">count is: {{ count }}</button>
+p Edit
+</template>
+
+<style scoped>
+a {
+	color: #42b983;
+}
+
+label {
+	margin: 0 0.5em;
+	font-weight: bold;
+}
+
+code {
+	background-color: #eee;
+	padding: 2px 4px;
+	border-radius: 4px;
+	color: #304455;
+}
+</style>
