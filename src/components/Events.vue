f76217dc7666161e2981933f7bcae3d247f3e866
Volkov Alexander <volkov.525@gmail.com>
2022-04-18 10:30:56 +0300

    add bar graph

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -26,7 +26,8 @@
 					VueApexCharts(type="donut" height="35" width="35" :options="row.options2" :series="row.series2" @click="showDialog2")
 
 	q-card()
-		VueApexCharts(type="radialBar" height="220" :options="chartOptions1" :series="series")
+		VueApexCharts(type="radialBar" height="220" :options="chartOptions1" :series="series" v-if="selection === 0")
+		VueApexCharts(type="bar" height="220" :options="barOptions" :series="barSeries" v-else)
 
 q-dialog(v-model="dialog1")
 	q-card(style="width: 900px; max-width: 80vw;")
@@ -161,6 +162,61 @@ const chartTableDonutBig = {
 		],
 	},
 }
+
+const barOptions = {
+	chart: {
+		type: 'bar',
+		height: 220,
+	},
+	plotOptions: {
+		bar: {
+			horizontal: false,
+			columnWidth: '55%',
+			endingShape: 'rounded',
+		},
+	},
+	dataLabels: {
+		enabled: false,
+	},
+	stroke: {
+		show: true,
+		width: 2,
+		colors: ['transparent'],
+	},
+	xaxis: {
+		categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+	},
+	yaxis: {
+		title: {
+			text: '$ (thousands)',
+		},
+	},
+	fill: {
+		opacity: 1,
+	},
+	tooltip: {
+		y: {
+			formatter: function (val) {
+				return '$ ' + val + ' thousands'
+			},
+		},
+	},
+}
+
+const barSeries = [
+	{
+		name: 'Net Profit',
+		data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
+	},
+	{
+		name: 'Revenue',
+		data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
+	},
+	{
+		name: 'Free Cash Flow',
+		data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
+	},
+]
 </script>
 
 <style scoped lang="scss">
