025c392537c19b5b84875395d3fd26ec2236e951
Volkov Alexander <volkov.525@gmail.com>
2022-04-19 19:39:57 +0300

    add filter to starred reports

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -3,28 +3,51 @@ q-page(padding)
 	.container
 		.row.justify-between.items-center
 			.zag Отчеты
-			chipCalendar(label="Прошлый месяц")
+			ChipCalendar(label="Прошлый месяц")
 		.grid
 			q-card
 			q-card
+				q-card-section
+					.row.justify-between.items-center
+						.subtitle-1
+							q-icon(name="mdi-star-outline").q-mr-sm
+							|Избранное
+						q-input(dense v-model="filter" clearable hide-bottom-space @clear="filter = ''")
+							template(v-slot:prepend)
+								q-icon(name="mdi-magnify")
+				q-card-section
+					q-chip(v-for="item in filteredReports" removable)
+						.ellipsis
+							|{{ item.label}}
+							q-tooltip(anchor="top middle" self="bottom middle") {{ item.label}}
 </template>
 
 <script setup lang="ts">
-import chipCalendar from '@/components/chipCalendar.vue'
+import { ref, computed } from 'vue'
+import ChipCalendar from '@/components/ChipCalendar.vue'
+import { starredReports } from '@/stores/data'
+
+const filter = ref('')
+
+const filteredReports = computed(() => {
+	return starredReports.filter((item) => item.label.includes(filter.value))
+})
 </script>
 
 <style scoped lang="scss">
-//@import '@/assets/css/colors.scss';
-.q-page {
-	background: pink;
-}
 .grid {
 	display: grid;
-	grid-template-columns: repeat(2, 1fr);
+	grid-template-columns: 1fr 2fr;
 	gap: 1rem;
-	div {
-		height: 100px;
-		background: white;
-	}
+}
+.q-input {
+	transform: translateY(-7px);
+	width: 230px;
+}
+.q-field--dense {
+	height: 32px;
+}
+.q-chip {
+	max-width: 200px;
 }
 </style>
