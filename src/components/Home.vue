6ef9ccb30168c8fbf83040deea51bb7849c57be9
Volkov Alexander <volkov.525@gmail.com>
2022-04-19 17:27:15 +0300

    bug with reports

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,16 @@ q-page(padding)
 	.container
 		.row.justify-between.items-center
 			.zag Тренды
-			chipCalendar
+			ChipCalendar
 		Trends1
 		.row.justify-between.items-center.q-mt-lg
 			.zag Категории
-			chipCalendar(label="Текущий месяц")
+			ChipCalendar(label="Текущий месяц")
 		Events
 
 		.row.justify-between.items-center.q-mt-lg
 			.zag Дополнительно
-			chipCalendar(label="Прошлый месяц")
+			ChipCalendar(label="Прошлый месяц")
 		Addition
 </template>
 
@@ -20,7 +20,7 @@ q-page(padding)
 import Trends1 from '@/components/Trends1.vue'
 import Events from '@/components/Events.vue'
 import Addition from '@/components/Addition.vue'
-import chipCalendar from '@/components/chipCalendar.vue'
+import ChipCalendar from '@/components/ChipCalendar.vue'
 </script>
 
 <style scoped lang="scss"></style>
---
+++ src/components/Reports1.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+q-page(padding)
+	.container
+		.row.justify-between.items-center
+			.zag Отчеты
+			ChipCalendar(label="Прошлый месяц")
+		.grid
+			q-card
+			q-card
+				q-card-section
+					.row.justify-between.items-center
+						.subtitle-1
+							q-icon(name="mdi-star-outline").q-mr-sm
+							|Избранное
+						q-input(dense v-model="filter" clearable hide-bottom-space)
+							template(v-slot:prepend)
+								q-icon(name="mdi-magnify" @click="test")
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import ChipCalendar from '@/components/ChipCalendar.vue'
+
+const filter = ref('')
+
+const test = () => {
+	console.log(1)
+}
+</script>
+
+<style scoped lang="scss">
+.grid {
+	display: grid;
+	grid-template-columns: 1fr 2fr;
+	gap: 1rem;
+}
+.q-input {
+	transform: translateY(-7px);
+	width: 250px;
+}
+.q-field--dense {
+	height: 32px;
+}
+</style>
