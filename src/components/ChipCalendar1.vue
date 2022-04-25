a0b9f5a96cb58edbfa9a65bf07e3953e1a39f740
Volkov Alexander <volkov.525@gmail.com>
2022-04-25 09:18:19 +0300

    fix mychip problem with name

--- src/components/ChipCalendar1.vue
+++ src/components/ChipCalendar1.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
 div
-	MyChip(:label="label")
+	MyChip1(:label="label")
 	q-btn(flat round icon="mdi-calendar" @click="toggle")
 </template>
 
 <script setup lang="ts">
 import { useStore } from '@/stores/store'
-import MyChip from '@/components/MyChip.vue'
+import MyChip1 from '@/components/MyChip1.vue'
 
 const props = defineProps({
 	label: {
