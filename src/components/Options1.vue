7fb21414e2fc447bebb05ae3474a313cd4d0352f
Volkov Alexander <volkov.525@gmail.com>
2022-04-25 11:55:37 +0300

    add options1

--- src/components/Options1.vue
+++ src/components/Options1.vue
@@ -9,18 +9,31 @@
 	div
 		.label Оператор
 		q-select(v-model="operModel"  :options="operator" filled dense bg-color="white")
+	div
+		.label Период
+		q-select(v-model="perModel"  :options="period" filled dense bg-color="white")
+	div
+		q-btn(flat round icon="mdi-calendar")
+			q-popup-proxy( cover transition-show="scale" transition-hide="scale")
+				q-date(v-model="date" range)
+					.row.items-center.justify-end.q-gutter-sm
+						q-btn(label="Cancel" color="primary" flat v-close-popup)
+						q-btn(label="OK" color="primary" flat v-close-popup)
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
 
 const region = ['Все', 'Не определен']
 const group = ['Все', 'Не все', 'Самые-самые']
 const operator = ['Все', 'Катя', 'Маша', 'Миша']
+const period = ['Последние 30 дней', 'Прошлый месяц', 'Текущий месяц', 'Сегодня']
 
 const regModel = ref('Все')
+const perModel = ref('Последние 30 дней')
 const groupModel = ref('Все')
 const operModel = ref('Все')
+const date = ref('2019/03/01')
 </script>
 
 <style scoped lang="scss">
@@ -29,7 +42,8 @@ const operModel = ref('Все')
 	margin-top: 1rem;
 	margin-bottom: 1rem;
 	display: grid;
-	grid-template-columns: repeat(4, 1fr);
+	grid-template-columns: 1fr 1fr 1fr 1fr 30px;
+	align-items: flex-end;
 	gap: 1rem;
 }
 .label {
