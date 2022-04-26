57763d34af0b3970423ac9c8385faf25dfed35bd
Volkov Alexander <volkov.525@gmail.com>
2022-04-26 08:30:16 +0300

    sort implemented

--- src/components/Stat.vue
+++ src/components/Stat.vue
@@ -7,26 +7,66 @@ q-expansion-item(v-model="stat").q-mt-md
 			.zag Статистика по ключевым словам
 	q-card-section.q-px-md.q-pt-none
 		.toolbar
-			q-select(label="Сортировка" v-model="sort" dense)
-			q-select(label="Часть речи" v-model="part" dense)
-			q-input(v-model="filter" dense)
+			q-select(label="Сортировка" v-model="sort" :options="sortModel" dense)
+			q-select(label="Часть речи" v-model="part" :options="partModel" dense)
+			q-input(v-model="filter" dense clearable)
 				template(v-slot:prepend)
 					q-icon(name="mdi-magnify")
 			q-range(v-model="label" :min="20" :max="400" :step="4" label color="primary" label-always)
 		.text-center
-			Wordchip(v-for="chip in chips" :key="chip.key" :label="chip.key" :count="chip.value" :part="chip.part")
+			component(:is="Wordchip" v-for="chip in mychips" :key="chip.key" :label="chip.key" :count="chip.value" :part="chip.part")
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref, computed } from 'vue'
 import Wordchip from '@/components/common/Wordchip.vue'
 import { chips } from '@/stores/chips'
 
 const stat = ref(true)
 const filter = ref('')
-const sort = ref('Алфавит А-Я')
+const sort = ref('Вес 9 -> 0')
 const part = ref('Существительные')
 const label = ref({ min: 50, max: 300 })
+
+const sortModel = ['Вес 9 -> 0', 'Вес 0 -> 9', 'Алфавит А -> Я', 'Алфавит Я -> А']
+const partModel = ['Существительные', 'Прилагательные', 'Глаголы', 'Наречия']
+
+const mychips = computed(() => {
+	switch (sort.value) {
+		case 'Вес 0 -> 9':
+			return chips.sort((a, b) => {
+				return a.value - b.value
+			})
+		case 'Вес 9 -> 0':
+			return chips.sort((a, b) => b.value - a.value)
+		case 'Алфавит А -> Я':
+			return chips.sort((a, b) => {
+				let fa = a.key.toLowerCase()
+				let fb = b.key.toLowerCase()
+				if (fa < fb) {
+					return -1
+				}
+				if (fa > fb) {
+					return 1
+				}
+				return 0
+			})
+		case 'Алфавит Я -> А':
+			return chips.sort((a, b) => {
+				let fa = a.key.toLowerCase()
+				let fb = b.key.toLowerCase()
+				if (fa > fb) {
+					return -1
+				}
+				if (fa < fb) {
+					return 1
+				}
+				return 0
+			})
+		default:
+			return chips
+	}
+})
 </script>
 
 <style scoped lang="scss">
