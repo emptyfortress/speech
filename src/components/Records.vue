934e97af5b217dab1a1c5071beae4078171d18cf
Volkov Alexander <volkov.525@gmail.com>
2022-04-27 09:45:16 +0300

    add Record component

---
+++ src/components/Records.vue
@@ -0,0 +1,104 @@
+<template lang="pug">
+q-expansion-item(v-model="rec").q-mt-md
+	template(v-slot:header)
+		q-item-section(avatar)
+			q-avatar(icon="mdi-record-circle-outline" flat)
+		q-item-section
+			.zag Записи сеансов обслуживания
+	q-card-section.q-px-md.q-pt-md
+		q-table(:rows="records"
+			:columns="columns"
+			selection="single"
+			:selected-rows-label="getSelectedString"
+			v-model:selected="selected"
+			rows-per-page-label="Записей на странице"
+			:rows-per-page-options='[10, 20, 50]'
+			@row-click="(evt, row, index) => select(row)").table
+			template(v-slot:body-selection)
+
+q-dialog(v-model="player" no-backdrop-dismiss no-shake seamless position="bottom").player
+	q-card(style="width: 650px")
+		q-linear-progress(:value="0.6" color="red")
+		q-card-section.row.items-center.no-wrap
+			div
+				.text-weight-bold {{ selected[0].date}}
+				.oper Оператор: {{ selected[0].operator}}
+			q-space
+			q-btn(flat round icon="mdi-rewind")
+			q-btn(flat round icon="mdi-pause")
+			q-btn(flat round icon="mdi-fast-forward")
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import type { Ref } from 'vue'
+import { records } from '@/stores/operators'
+import { useStore } from '@/stores/store'
+
+interface RecordColumn {
+	name: string
+	label: string
+	field: string | ((row: any) => any)
+	required?: boolean
+	align?: 'left' | 'right' | 'center'
+	sortable?: boolean
+	sort?: (a: any, b: any, rowA: any, rowB: any) => number
+	sortOrder?: 'ad' | 'da'
+	format?: (val: any, row: any) => any
+	style?: string | ((row: any) => string)
+	classes?: string | ((row: any) => string)
+	headerStyle?: string
+	headerClasses?: string
+}
+
+interface Row {
+	id: number
+	date: string
+	group: string
+	record: string
+	operator: string
+	client: string
+}
+
+const mystore = useStore()
+const rec = ref(true)
+const selected: Ref<Row[]> = ref([])
+const player = computed(() => {
+	if (selected.value.length) return true
+	return false
+})
+const select = (e: Row) => {
+	if (selected.value.length === 0) {
+		selected.value.push(e)
+	} else if (selected.value[0].id === e.id) {
+		selected.value = []
+	} else {
+		selected.value = []
+		selected.value.push(e)
+	}
+}
+const getSelectedString = (e: number) => {
+	return `Выбрана ${e} запись`
+}
+
+const columns: RecordColumn[] = [
+	{ name: 'date', label: 'Дата, время', align: 'left', field: 'date', sortable: true },
+	{ name: 'operator', label: 'Оператор', align: 'left', field: 'operator', sortable: true },
+	{ name: 'client', label: 'Клиент', align: 'left', field: 'client', sortable: true },
+	{ name: 'group', label: 'Группа', align: 'left', field: 'group', sortable: true },
+	{ name: 'record', label: 'Запись', align: 'left', field: 'record', sortable: true },
+]
+</script>
+
+<style scoped lang="scss">
+//@import '@/assets/css/colors.scss';
+.player .q-card {
+	// background: $light-blue-10;
+	background: #394b54;
+	background: $blue-grey-9;
+	color: white;
+	.oper {
+		color: #6d8e9e;
+	}
+}
+</style>
