5a7172373047be1580815920d1dfe9d91f61b8b5
Volkov Alexander <volkov.525@gmail.com>
2022-07-04 11:35:18 +0300

    profile page in progress

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -9,32 +9,52 @@ q-page(padding)
 					.text-weight-bold Голомудько
 					.name Разубай Буранович
 				q-space
-				q-btn(label="Редактировать" unelevated color="primary")
+				q-btn(label="Выйти" unelevated color="primary")
+			q-btn(color="white" round icon="mdi-camera" text-color="black" size="sm").photo
 		br
 		br
 		br
 		br
 		q-tabs(v-model="tab" dense active-color="primary" indicator-color="primary" align="left" )
-			q-tab(name="one" label="Информация")
-			q-tab(name="two" label="Настройки")
-			q-tab(name="three" label="Безопасность")
+			q-tab(name="info" label="Информация")
+			q-tab(name="setup" label="Настройки")
+			q-tab(name="notific" label="Уведомления")
+			q-tab(name="login" label="Безопасность")
 		q-separator
+
+		q-tab-panels(v-model="tab" animated)
+			q-tab-panel(name="info")
+				.mygrid
+					.label fij
+					.value lajsdlaj
+					q-btn(flat round icon="mdi-pencil-outline")
+			q-tab-panel(name="setup")
+				.text-h6 one
+			q-tab-panel(name="notific")
+				.text-h6 one
+			q-tab-panel(name="login")
+				.text-h6 one
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
 
-const tab = ref('one')
+const tab = ref('info')
 </script>
 
 <style scoped lang="scss">
-//@import '@/assets/css/colors.scss';
+@import '@/assets/styles/myvariables.scss';
 .huge {
 	height: 100px;
-	background: url(@/assets/img/pult.jpg);
+	background: url(@/assets/img/abstract.jpg);
 	background-size: cover;
 	background-position: 0 30%;
 	position: relative;
+}
+.photo {
+	position: absolute;
+	bottom: -70px;
+	left: 40px;
 }
 .ava {
 	height: 100px;
@@ -54,5 +74,14 @@ const tab = ref('one')
 		background: $blue;
 		border: 2px solid white;
 	}
+}
+.mygrid {
+	display: grid;
+	grid-template-columns: 300px 200px 40px;
+	gap: 1rem;
+}
+.q-tab-panels {
+	min-height: 500px;
+	box-shadow: $card-shadow;
 }
 </style>
