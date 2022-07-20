0465170d75b7de1c81ddee616e238779b2f59cef
Volkov Alexander <volkov.525@gmail.com>
2022-07-20 14:09:48 +0300

    all seems working

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,24 +1,21 @@
-<template>
-	<Tree :value="treeData" :indent="40" v-slot="{ node, index, path, tree }">
-		<span>
-			<b @click="tree.toggleFold(node, path)">
-				{{ node.$folded ? '+' : '-' }}
-			</b>
-			{{ node.title }}
-		</span>
-	</Tree>
+<template lang="pug">
+q-page(padding)
+	.container
+		component(:is="Draggable" :flatData="flatData" idKey="id" parentIdKey="pid")
+			template(v-slot="{ node, tree }")
+				b(@click="tree.toggleFold(node)") {{ node.$folded ? '+' : '-' }}
+				span {{ node.text }}
 </template>
 
-<script lang="ts">
-import 'he-tree-vue/dist/he-tree-vue.css'
-import { Tree, Fold, Draggable } from 'he-tree-vue'
+<script setup lang="ts">
+import '@he-tree/vue3/dist/he-tree-vue3.css'
+import { Draggable } from '@he-tree/vue3'
 
-export default {
-	components: { Tree: Tree.mixPlugins([Fold, Draggable]) },
-	data() {
-		return {
-			treeData: [{ title: 'node 1' }, { title: 'node 2', children: [{ title: 'node 2-1' }] }],
-		}
-	},
-}
+const flatData = [
+	{ text: 'node1', id: 1 },
+	{ text: 'node2', id: 2 },
+	{ text: 'node1-1', id: 3, pid: 1 },
+	{ text: 'node1-2', id: 4, pid: 1 },
+	{ text: 'node1-3', id: 5, pid: 1 },
+]
 </script>
