91a44214236b4c8c5fd86cbd882664f38c87d4af
Volkov Alexander <volkov.525@gmail.com>
2022-10-18 13:38:23 +0300

    new branch for tree in categ

--- src/components/CategNew.vue
+++ src/components/CategNew.vue
@@ -1,183 +1,74 @@
 <template lang="pug">
-.gridcat
-	q-table(:columns="columns"
-		ref="table"
-		:rows="rows"
-		no-data-label="Категории не настроены"
-		:rows-per-page-options="[0]"
-		).table
-		template(v-slot:top)
-			.zag.cursor-pointer
-				|{{cat.catselect}}
-				q-icon(name="mdi-menu-down")
-				q-menu
-					q-list
-						template(v-for="item in cat.cat[0].children" :key="item.id" )
-							q-item(clickable v-close-popup @click="selectCat(item.label)" :class="{'selected' : item.label === cat.catselect}")
+q-expansion-item(v-model="tree")
+	template(v-slot:header)
+		q-item-section(avatar).line
+			q-avatar(icon="mdi-file-tree" flat)
 								q-item-section
-									q-item-label {{ item.label }}
-		template(v-slot:body="props")
-			q-tr(:props="props" @click="showPodcat(props.row.label)" ).cursor-pointer
-				q-td(key="label" :props="props") {{ props.row.label }}
-				q-td(key="call" :props="props") {{ props.row.call }}
-				q-td(key="ant" :props="props") {{ props.row.ant }}
-				q-td(key="loud" :props="props") {{ props.row.loud }}
-				q-td(key="interrupt" :props="props") {{ props.row.interrupt }}
+			.zag Дерево категорий
+q-splitter(v-model="splitterModel" style="height: 490px;")
+	template(v-slot:before)
+		q-card.left
+			.top
+				q-btn(round flat dense icon="mdi-unfold-more-horizontal")
+					q-tooltip Развернуть
+				q-input(dense ref="input" debounce="0" hide-bottom-space color="primary" autofocus v-model="filter" clearable @clear="filter = ''").input
+					template(v-slot:prepend)
+						q-icon(name="mdi-magnify")
 
-	q-card.graph
-		.flex.flex-center.full-height(v-if="rows.length === 0")
-			q-icon(name="mdi-emoticon-poop" size="48px")
-		component(v-else ref="chart" :is="VueApexCharts" type="bar" :options="coolOptions" :series="coolSeries" :height="calcHeight").chh
+			q-card-section
+				q-scroll-area.scroll
+					q-tree(:nodes="cat.cat"
+						node-key="id"
+						no-results-label="Ничего нет"
+						selected-color="primary"
+						no-selection-unset
+						v-model:selected="selected"
+						v-model:expanded="expanded"
+						:filter="filter").cat
+						template(v-slot:default-header="prop")
+							.nod
+								component(:is="WordHighlighter" :query="filter") {{prop.node.label}}
 
-component(:is="DialogPodcat" :show="bigTable" :category="categoryName" @close="bigTable = false")
+	template(v-slot:after)
+		q-card.right
+			p lakjsldj
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import type { Ref } from 'vue'
 import { useCat } from '@/stores/category1'
-import VueApexCharts from 'vue3-apexcharts'
+import WordHighlighter from 'vue-word-highlighter'
 import { randomArray } from '@/utils/utils'
-import DialogPodcat from '@/components/common/DialogPodcat.vue'
 
 const cat = useCat()
-const table = ref()
+const selected = ref(cat.cat[0].id)
+const expanded: Ref<string[]> = ref(['0', '1'])
 
-const chart = ref()
-
-const selectCat = (label: string) => {
-	cat.setCatselect(label)
-}
-
-const calcHeight = computed(() => {
-	let rows = cat.rowNum
-	return rows * 50 + 42 + 'px'
-})
-
-const rows = computed(() => {
-	let cur = cat.cat[0].children.filter((e: any) => e.label === cat.catselect)
-	return cur[0].children
-})
-
-const columns: Column[] = [
-	{ name: 'label', label: 'Категории', field: 'label', sortable: true, align: 'left' },
-	{ name: 'call', label: 'Звонки', field: 'call', sortable: true, align: 'right' },
-	{
-		name: 'ant',
-		label: 'АНТ, мин',
-		field: 'ant',
-		sortable: true,
-		align: 'right',
-	},
-	{
-		name: 'loud',
-		label: 'Тишина, %',
-		field: 'loud',
-		sortable: true,
-		align: 'right',
-	},
-	{
-		id: 4,
-		name: 'interrupt',
-		label: 'Перебивания, %',
-		field: 'interrupt',
-		sortable: true,
-		align: 'right',
-	},
-]
-
-const coolOptions = computed(() => {
-	return {
-		chart: {
-			type: 'bar',
-			stacked: true,
-			toolbar: {
-				show: false,
-			},
-		},
-		plotOptions: {
-			bar: {
-				horizontal: true,
-			},
-		},
-		stroke: {
-			width: 1,
-			colors: ['#fff'],
-		},
-		xaxis: {
-			categories: cat.getSubCat,
-			labels: {
-				show: false,
-				minHeight: 0,
-				maxHeight: 0,
-			},
-		},
-		yaxis: {
-			show: false,
-			title: {
-				text: undefined,
-			},
-		},
-		fill: {
-			opacity: 1,
-		},
-		legend: {
-			show: true,
-			position: 'top',
-			horizontalAlign: 'center',
-		},
-		colors: ['#29A1F9', '#FDB948', '#C72829'],
-	}
-})
-
-const coolSeries = computed(() => {
-	return [
-		{
-			name: 'AHT',
-			data: randomArray(cat.rowNum, 10, 40),
-		},
-		{
-			name: 'Тишина',
-			data: randomArray(cat.rowNum, 3, 15),
-		},
-		{
-			name: 'Перебивания',
-			data: randomArray(cat.rowNum, 7, 20),
-		},
-	]
-})
-
-const bigTable = ref(false)
-const categoryName = ref('')
-const showPodcat = (e: string) => {
-	bigTable.value = true
-	categoryName.value = e
-}
+const splitterModel = ref(30)
+const tree = ref(true)
+const filter = ref('')
 </script>
 
 <style scoped lang="scss">
 @import '@/assets/styles/myvariables.scss';
-//@import '@/assets/css/colors.scss';
-.gridcat {
-	margin-top: 1rem;
-	margin-left: 1rem;
-	margin-right: 1rem;
-	display: grid;
-	grid-template-columns: repeat(5, 1fr);
-	column-gap: 1rem;
-	align-content: start;
+
+.top {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem;
+	padding-bottom: 0;
 }
-.table {
-	grid-column: 1/4;
+.q-card {
+	margin: 0.5rem;
 }
-.graph {
-	grid-column: 4/6;
+.razv {
+	font-size: 0.7rem;
+	color: #666;
+	cursor: pointer;
 }
-.selected {
-	background: $bgSelection;
-	color: $blue-9;
-}
-.chh {
-	margin-top: 51px;
-	// background: pink;
+.scroll {
+	height: 380px;
 }
 </style>
