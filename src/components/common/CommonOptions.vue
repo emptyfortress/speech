cf5425ac7550609cac6664a64f2f44a2904d0b3e
Volkov Alexander <volkov.525@gmail.com>
2022-10-21 15:22:37 +0300

    fix comment with custom range date

--- src/components/common/CommonOptions.vue
+++ src/components/common/CommonOptions.vue
@@ -1,21 +1,27 @@
 <template lang="pug">
 .grid
-	MySelect(label="Регион" :model="regModel" :options="region")
-	MySelect(label="Группа" :model="groupModel" :options="group")
-	MySelect(label="Оператор" :model="operModel" :options="operator")
-	MySelect(label="Период" :model="perModel" :options="period")
+	component(:is="MySelect" label="Регион" :model="regModel" :options="region")
+	component(:is="MySelect" label="Группа" :model="groupModel" :options="group")
+	component(:is="MySelect" label="Оператор" :model="operModel" :options="operator")
+	component(:is="MySelect" label="Период" :model="perModel" :options="period")
 	div
 		q-btn(flat round icon="mdi-calendar")
 			q-popup-proxy( cover transition-show="scale" transition-hide="scale")
 				q-date(v-model="date" range)
 					.row.items-center.justify-end.q-gutter-sm
 						q-btn(label="Cancel" color="primary" flat v-close-popup)
-						q-btn(label="OK" color="primary" flat v-close-popup)
+						q-btn(label="OK" color="primary" flat v-close-popup @click="setRange")
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import type { Ref } from 'vue'
 import MySelect from '@/components/common/MySelect.vue'
+
+interface Range {
+	from: String
+	to: String
+}
 
 const region = ['Все', 'Не определен']
 const group = ['Все', 'Не все', 'Самые-самые']
@@ -23,10 +29,15 @@ const operator = ['Все', 'Катя', 'Маша', 'Миша']
 const period = ['Последние 30 дней', 'Прошлый месяц', 'Текущий месяц', 'Сегодня']
 
 const regModel = ref('Все')
-const perModel = ref('Последние 30 дней')
+const perModel: Ref<Range | String> = ref('Последние 30 дней')
 const groupModel = ref('Все')
 const operModel = ref('Все')
 const date = ref('2019/03/01')
+
+const setRange = () => {
+	let temp = Object.values(date.value)
+	perModel.value = temp[0] + ' -- ' + temp[1]
+}
 </script>
 
 <style scoped lang="scss">
